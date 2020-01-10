a7aa8232b1b17e77d90158078c2643bf8afa65a4
Calixte Denizet <calixte.denizet@gmail.com>
2020-01-10 17:52:57 +0100

    Remove tracemalloc stuff and set max-requests to 10

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn -b 0.0.0.0:$PORT --limit-request-line 8190 --max-requests 256 crashstop:app 
+web: gunicorn -b 0.0.0.0:$PORT --limit-request-line 8190 --max-requests 10 --workers=3 crashstop:app
 clock: python bin/schedule.py
--- crashstop/__init__.py
+++ crashstop/__init__.py
@@ -6,9 +6,7 @@ from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 import os
 from . import config
-import tracemalloc
 
-tracemalloc.start()
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -29,16 +27,7 @@ def setup():
 
 @app.route('/sumup.html')
 def sumup_html():
-    s1 = tracemalloc.take_snapshot()
-
-    r = html.sumup()
-
-    s2 = tracemalloc.take_snapshot()
-
-    for i in s2.compare_to(s1,'lineno')[:10]:
-        print(i)
-
-    return r
+    return html.sumup()
 
 
 @app.route('/')
